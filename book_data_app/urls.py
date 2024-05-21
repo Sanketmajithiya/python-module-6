a478ed327da1d698dd05e3d010a280568ed93823
Sanketmajithiya <Sanketmajithiya@gmail.com>
2024-05-21 12:40:47 +0530

    #

---
+++ book_data_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', BookListAPI, name='BookListAPI'),
+    path('BookDetailAPI/<int:isbn>', BookDetailAPI, name='BookDetailAPI'),
+]
+
+
